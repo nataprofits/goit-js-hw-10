88f4be1efbb8d8403eb6fccf50667d45109179ec
nataprofits <nataprofits@gmail.com>
2023-04-19 21:21:05 +0100

    time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
-const DEBOUNCE_DELAY = 5000;
+const DEBOUNCE_DELAY = 300;
 
 
 // Инициализация библиотеки
